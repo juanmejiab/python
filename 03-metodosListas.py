79e1fb56dbcef50ce929538a268bbb12fc3b5a7c
Juan Pablo Mejia <juanmejiab02gmail.com>
2023-05-08 21:57:03 -0500

    Se agrego el arhivo metodos listas

---
+++ 03-metodosListas.py
@@ -0,0 +1,23 @@
+listas = ['Juan', 8, 'Pedro'] #Puede tener numeros o strings
+listas.append(8.99) #Añadir elementos a la lista
+lista2 = listas.copy() #Copiar lista
+print(listas, lista2)
+
+lista2.append(8)
+lista2.count(8) #Contar un elemento dado en la lista
+len(lista2) #Obtener la longitud de una lista
+len(listas)
+listas[0] #Obtener el elemento de una lista seginm
+
+listas.pop() #Eliminar el ultimo elemento de una lista
+listas
+
+listas.remove('Juan') #Eliminar un elemento especifico de la lista
+listas
+
+listas.reverse() #Ordena de atras para adelante una lista
+listas
+
+lista3 = [5,10,1,0.01,22]
+lista3.sort() #Ordena la lista de menor a mayor
+lista3
