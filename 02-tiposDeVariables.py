41849b4b4455c1b50aa54689a129752c6b5add8e
Juan Pablo Mejia <juanmejiab02gmail.com>
2023-05-08 21:56:42 -0500

    Se agrego el arhivo tipos de variables

---
+++ 02-tiposDeVariables.py
@@ -0,0 +1,29 @@
+x = 5 #Entero
+y = 'Esto es un string' #String
+z = 5.676 #Float\
+
+print(x,y,z)
+
+listas = ['Juan', 8, 'Pedro'] #Puede tener numeros o strings
+
+
+tupla = ('Juan', 8, 'Mejia')
+
+
+diccionario = {
+    "nombre":"juan",
+    "apellido":"perez",
+    "edad":8
+}
+
+# Diccionario anidado
+personas = {
+    "Juan": {
+        "nombre":"Juan Gomez",
+        "edad":8
+    },
+    "Pedro":{
+        "nombre":"Pedro Gonzalez",
+        "edad":12
+    }
+}
