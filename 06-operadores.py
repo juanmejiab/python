eb0e4ef3dfa06dff2bab2bd18b21b288adf4b6ac
Juan Pablo Mejia <juanmejiab02gmail.com>
2023-05-10 19:39:03 -0500

    Modificaciones al archivo de operadores

---
+++ 06-operadores.py
@@ -0,0 +1,22 @@
+# Operadores artitmeticos
+a = 30
+b = 5
+
+print(a + b) # Suma
+print(a - b) # Resta
+print(a * b) # Multiplicacion
+print(a / b) # Division
+print(a // b) # Division entera
+print(a % b) # Modulo
+print(a ** b) # Potenciacion
+
+
+# Operadores de comparacion
+print(a == b) # Igualdad
+print(a != b) # Diferencia
+print(a < b) # Menor que
+print(a > b) # Mayor que
+print(a <= b) # Menor o igual que
+print(a >= b) # Mayor o igual que
+
+
