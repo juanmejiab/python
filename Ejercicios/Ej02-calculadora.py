32b907b29962169c487c940812a92ae408771915
Juan Pablo Mejia <juanmejiab02gmail.com>
2023-05-21 19:44:02 -0500

    Realización del ejercicio 2

---
+++ Ejercicios/Ej02-calculadora.py
@@ -0,0 +1,30 @@
+# Calculadora que recibe por consola
+# la operación a realizar y dos números
+
+y = []
+operando = ''
+
+for x in range(1, 3):
+    y.append(int(input(
+        f'Ingrese el número {x}: '))
+    )
+
+operando = input('Ingrese la operación: ')
+
+
+def operaciones(operador):
+    if operador == '+':
+        print('El resultado es:', y[0] + y[1])
+    elif operador == '-':
+        print('El resultado es:', y[0] - y[1])
+    elif operador == '*':
+        print('El resultado es:', y[0] * y[1])
+    elif operador == '/':
+        print('El resultado es:', y[0] / y[1])
+    else:
+        print('Opción inválida.')
+        op = input('Ingrese la operación: ')
+        operaciones(op)
+
+
+operaciones(operando)
