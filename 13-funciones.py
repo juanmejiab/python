46c48bd4a6041d7adeb3055d8c9def3313418967
Juan Pablo Mejia <juanmejiab02gmail.com>
2023-05-23 18:34:09 -0500

    Mas ejemplos sobre funciones

--- 13-funciones.py
+++ 13-funciones.py
@@ -37,13 +37,73 @@ def sumatoria(*numeros: int) -> int:
 sumatoria(1, 5, 6, 9, 10, 14, 12, 11, 10, 20, 35, 28)
 
 # -------------Ejemplo 4-----------------
+# Uso de kwargs, usa la sintaxis de diccionario
+# y retorna un diccionario
 
 
-def nombre_completo(**kwargs: dict):
-    print(kwargs['nombre'], kwargs['apellido'])
+def nombre_completo(**kwargs: dict) -> dict:
+    return (kwargs['nombre'], kwargs['apellido'])
 
 
-nombre_completo(
+datos = nombre_completo(
     nombre='Juan',
     apellido='Mejia'
 )
+
+# -------------Ejemplo 5-----------------
+# Valor de parametro por defecto
+# Al tenerlo por defeto no es obligatiorio
+# ingresarlos todos por defecto
+
+
+def multiplicacion(x: int, y: int, z=2) -> int:
+    print(x*y*z)
+
+
+multiplicacion(1, 2, 4)
+
+# Uso de la palabra RETURN
+# -------------Ejemplo 1-----------------
+# Funcion que retorna la sumatoria de x numeros
+
+
+def multiplicacion(*numeros: int) -> int:
+    mult = 1
+    for x in numeros:
+        mult *= x
+    return mult
+
+
+resultado_mult = multiplicacion(1, 5, 3, 6, 9, 7, 10)
+print(f'El resultado de la multiplicacion es {resultado_mult}')
+
+# -------------Ejemplo 2-----------------
+# Funcion que retorna los datos de una persona
+
+
+def datos_usr(**kwargs: dict) -> dict:
+    return kwargs
+
+
+datos = datos_usr(
+    nombre='Juan',
+    apellido="Mejia",
+    telefono='3102221234'
+)
+
+print('Mi nombre es %s, mi apellido %s y mi telefono %s' %
+      (datos['nombre'], datos['apellido'], datos['telefono']))
+
+# -------------Recursion---------------
+# Consiste en invocar una funcion dentro
+# de si misma
+
+
+def recursion(i: int) -> int:
+    if (i < 2):
+        return i
+    print(i)
+    recursion(i-1)
+
+
+recursion(5)
