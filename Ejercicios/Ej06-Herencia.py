78feea4dab3feac4f772a9d098eab7d7ecd892e8
Juan Pablo Mejia <juanmejiab02gmail.com>
2023-05-24 21:32:31 -0500

    Ejercicio sobre POO y herencia

---
+++ Ejercicios/Ej06-Herencia.py
@@ -0,0 +1,27 @@
+class Animal:
+    def __init__(self, nombre: str, raza: str) -> None:
+        self.nombre = nombre
+        self.raza = raza
+
+    def saludar(self) -> str:
+        return (f'Hola! Mi nombre es {self.nombre}, soy un {self.tipo} '
+                f'y mi raza es {self.raza}')
+
+
+class Gato(Animal):
+    tipo = 'Gato'
+
+    def __init__(self, nombre: str, raza: str,
+                 onomatopeya: str) -> None:
+        Animal.__init__(self, nombre, raza)
+        self.onomatopeya = onomatopeya
+
+    def maullar(self) -> str:
+        return f'Soy {self.nombre} y mi sonido es {self.onomatopeya}'
+
+
+class Perro(Animal):
+    tipo = 'Perro'
+
+
+labrador = Perro('Rush', 'Labrador')
