6cbf9eff0c28b170d369c455baca35cbb74957ca
Juan Pablo Mejia <juanmejiab02gmail.com>
2023-05-25 20:14:29 -0500

    Ejemplo de operadores de comparacion concatenados

--- 09-condicionales.py
+++ 09-condicionales.py
@@ -25,3 +25,12 @@ elif x == y:
     print(f'{x} es igual a {y}')
 else:
     print(f'{x} es menor que y')
+
+# Concatenacion de operadores de comparacion
+# Ejemplo: validar que la edad de una persona este entre los 18 y 65
+edad = 20
+
+if 18 <= edad < 65:
+    print('La persona esta en el rango definido')
+else:
+    print('La persona no esta en el rango definido')
