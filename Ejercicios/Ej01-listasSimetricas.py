ed2730f1b7acfb4c74fdf6845285f750557bb1eb
Juan Pablo Mejia <juanmejiab02gmail.com>
2023-05-26 17:19:31 -0500

    Cambio de nombre de algunas variables

--- Ejercicios/Ej01-listasSimetricas.py
+++ Ejercicios/Ej01-listasSimetricas.py
@@ -1,15 +1,17 @@
-myArray = ['a','b','d','s','d','3','a']
+myArray = ['a', 'b', 'd', 's', 'd', '3', 'a']
 
-def is_symetric(arreglo:list) -> str:
-    copia_myArray = arreglo.copy()
-    copia_myArray.reverse()
-    x=0
 
-    while x < len(arreglo):
-        if copia_myArray[x] != arreglo[x]:
+def is_symetric(array: list) -> str:
+    myArray_copy = array.copy()
+    myArray_copy.reverse()
+    x = 0
+
+    while x < len(array):
+        if myArray_copy[x] != array[x]:
             return 'Asymetric'
-        x+=1
-        
+        x += 1
+
     return 'Symetric'
+
 
 print(is_symetric(myArray))
